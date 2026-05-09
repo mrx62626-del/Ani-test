985323a63cba9e5281dca1de8621f1d071e9fb2f
Onliserve <mrx62626@gmail.com>
2026-05-09 19:18:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,18 @@ export default defineConfig({
   base: '/',
   envDir: './backend',
   plugins: [react()],
+
   server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+    allowedHosts: true,
+
     proxy: {
       '/avatars': {
         target: 'http://localhost:3001',
         changeOrigin: true,
       },
+
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
